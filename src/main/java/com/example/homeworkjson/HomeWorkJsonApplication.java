702c901f301024ae0d5a5686892fdc4ff20c3ae5
Ani <ani-alaverdyan@mail.ru>
2022-11-24 01:28:14 +0300

    commit

---
+++ src/main/java/com/example/homeworkjson/HomeWorkJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkJsonApplication.class, args);
+    }
+
+}
